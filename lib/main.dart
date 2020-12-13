5d9d1c615aedf8b9943f02fb5168ab1c64a78a69
ninochang <nino.chang@swag.live>
2020-12-13 17:09:26 +0800

    Change Game to statefule widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,33 +2,35 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  // runApp(MyApp());
+  runApp(Game());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Home(),
-      appBar: AppBar(title: Text('Home')),
-    ));
-  }
-}
+// ** Home
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//       body: Home(),
+//       appBar: AppBar(title: Text('Home')),
+//     ));
+//   }
+// }
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-        child: ElevatedButton(
-      onPressed: () {
-        Navigator.push(
-            context, MaterialPageRoute(builder: (context) => Game()));
-      },
-      child: Text('Go to Game'),
-    ));
-  }
-}
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Center(
+//         child: ElevatedButton(
+//       onPressed: () {
+//         Navigator.push(
+//             context, MaterialPageRoute(builder: (context) => Game()));
+//       },
+//       child: Text('Go to Game'),
+//     ));
+//   }
+// }
 
 class GameRoute extends StatelessWidget {
   @override
@@ -44,126 +46,57 @@ class GameRoute extends StatelessWidget {
 }
 
 /// This is Snake Game application widget.
-class Game extends StatelessWidget {
-  static const String _title = 'Game Route';
+// class Game extends StatelessWidget {
+class Game extends StatefulWidget {
+  // static const _title = 'Game Route';
+  // const Game({Key key}) : super(key: key);
+  final gridCount = 500;
+  var gridColor = Colors.grey;
+
+  @override
+  _GameState createState() => _GameState();
+}
+
+class _GameState extends State<Game> {
+  final gridCount = 500;
+  var gridColor = Colors.grey;
+  Timer _everySecond;
+
+  @override
+  Widget initState() {
+    super.initState();
+
+    gridColor = gridColor == Colors.grey ? Colors.green : Colors.grey;
+    _everySecond = Timer.periodic(Duration(seconds: 1), (Timer t) {
+      setState(() {
+        _onTimerTick();
+      });
+    });
+  }
+
+  void _onTimerTick() {
+    setState(() {
+      gridColor = gridColor == Colors.grey ? Colors.green : Colors.grey;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    int gridCount = 500;
-
-    Widget GameWidget() {
-      // return GridView.count(
-      //     padding: const EdgeInsets.all(5.0),
-      //     crossAxisCount: 20,
-      //     mainAxisSpacing: 2.0,
-      //     crossAxisSpacing: 2.0,
-      //     childAspectRatio: 1.0,
-      //     children: List.generate(gridCount, (index) {
-      //       return Container(height: 1, width: 1, color: Colors.grey);
-      //     }));
-
-      return Container(
-          child: Column(children: [
-        GridView.count(
-            padding: const EdgeInsets.all(5.0),
-            crossAxisCount: 20,
-            mainAxisSpacing: 2.0,
-            crossAxisSpacing: 2.0,
-            childAspectRatio: 1.0,
-            children: List.generate(gridCount, (index) {
-              return Container(height: 1, width: 1, color: Colors.grey);
-            }))
-      ]));
-    }
-
     return MaterialApp(
-      title: _title,
-      home: Scaffold(
-          appBar: AppBar(title: const Text(_title)),
-          // body: GridView.count(
-          //     padding: const EdgeInsets.all(5.0),
-          //     crossAxisCount: 20,
-          //     mainAxisSpacing: 2.0,
-          //     crossAxisSpacing: 2.0,
-          //     childAspectRatio: 1.0,
-          //     children: List.generate(gridCount, (index) {
-          //       return Container(height: 1, width: 1, color: Colors.grey);
-          //     }))
-          body: Row(
-            children: [
-              // GridView.count(
-              //     padding: const EdgeInsets.all(5.0),
-              //     crossAxisCount: 20,
-              //     mainAxisSpacing: 2.0,
-              //     crossAxisSpacing: 2.0,
-              //     childAspectRatio: 1.0,
-              //     semanticChildCount: gridCount,
-              //     children: List.generate(gridCount, (int index) {
-              //       return Container(height: 1, width: 1, color: Colors.grey);
-              //     })),
-              Text('asasdfdsf'),
-              Text('asasdfdsfsdfsdfsdfsdf'),
-            ],
-          )
-          // body: GameWidget(),
-          ),
-    );
+        home: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          GridView.count(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(5.0),
+              crossAxisCount: 20,
+              mainAxisSpacing: 2.0,
+              crossAxisSpacing: 2.0,
+              childAspectRatio: 1.0,
+              children: List.generate(gridCount, (index) {
+                return Container(height: 1, width: 1, color: gridColor);
+              }))
+        ])));
   }
 }
-
-/// This is the stateful widget that the main application instantiates.
-// class MyStatefulWidget extends StatefulWidget {
-//   MyStatefulWidget({Key key}) : super(key: key);
-
-//   @override
-//   _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-// }
-
-// /// This is the private State class that goes with MyStatefulWidget.
-// class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-//   double position = 1;
-//   double _left = 1;
-//   bool _isFavorited = true;
-//   int _favoriteCount = 41;
-
-//   Widget first_row() {
-//     List<int> text = [1, 2, 3, 4, 5];
-//     return Column(children: [for (var i in text) Text(i.toString())]);
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(
-//       mainAxisSize: MainAxisSize.min,
-//       children: [
-//         first_row(),
-//         Container(
-//           padding: EdgeInsets.all(0),
-//           child: IconButton(
-//             icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
-//             color: Colors.red[500],
-//             onPressed: _toggleFavorite,
-//           ),
-//         ),
-//         Container(
-//           width: 18,
-//           child: Text('$_favoriteCount'),
-//         ),
-//       ],
-//     );
-//   }
-
-//   void _toggleFavorite() {
-//     var period = const Duration(seconds: 1);
-//     Timer.periodic(period, (timer) {
-//       if (_isFavorited) {
-//         _favoriteCount -= 1;
-//         _isFavorited = false;
-//       } else {
-//         _favoriteCount += 1;
-//         _isFavorited = true;
-//       }
-//       setState(() {});
-//     });
-//   }
-// }
